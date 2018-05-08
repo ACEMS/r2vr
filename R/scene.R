954abaa707a57996685461493abd2b998db309f1
Miles McBain <=>
2018-05-08 16:46:25 +1000

    Filled out Scene class rendering. Added scene tests.
    Scene inherits from entity.
    Added test for rendering a basic scene template.
    Added unit test for determining indentation levels within the template.

--- R/scene.R
+++ R/scene.R
@@ -1,11 +1,97 @@
-A_Scene <- R6::R6Class("A_Scene",
-                       public = list(
-                         template = NULL,
-                         components = NULL,
-                         children = NULL,
-                         assets = NULL,
-                         sources = NULL,
-                         initialize = function(template = "basic", ...){
-                         }
-                       )
-            )
+A_Scene <-
+  R6::R6Class("A_Scene",
+              inherit = A_Entity,
+              public = list(
+                template = NULL,
+                initialize = function(template = "basic", ...){
+
+                  ## If template is not a file, assume it is a built-in
+                  if (!file.exists(template)){
+                    template_file <-
+                      paste0(file.path("inst","templates",template),".html")
+                    template_file_path <- system.file(template_file, package = "r2vr")
+                    if (!file.exists(template_file_path)){
+                      stop("A-Frame Scene template: ", template ,", not found")
+                    }
+                  } else{
+                    template_file_path <- template
+                  }
+                  self$template <- readr::read_file(template_file_path)
+
+                  ## Call constructor for A_Entity
+                  super$initialize(...)
+                },
+
+                render = function(){
+                  template_env <- new.env()
+
+                  ## scene components
+                  if (length(self$components > 0)) {
+                    template_env$scene_components <-
+                      paste0(self_render_component_list(), collapse = " ")
+                  } else {
+                    template_env$scene_components <- ""
+                  }
+
+                  ## entities
+                  if (length(self$children) > 0){
+                    ## ask children to render
+                    child_tags <-
+                      purrr::map(self$children, ~.render())
+
+                    ## indent them to correct position in template
+                    ## paste into single block
+                    template_env$entities <-
+                      indent_to_level(paste0(child_tags, collapse = "\n"),
+                                      "entities")
+
+                  } else {
+                    template_env$entities <- ""
+                  }
+
+                  ## assets
+                  if(length(self$assets > 0)){
+                    ## ask assets to render
+                    asset_tags <-
+                      purrr::map(self$assets, ~.$render())
+
+                    template_env$assets <-
+                      indent_to_level(paste0(asset_tags, collapse = "\n"),
+                                      "assets")
+                  } else {
+                    template_env$assets <- ""
+                  }
+
+                  ## sources
+                  if(length(self$sources > 0)){
+                    source_tags <-
+                      paste0("<script crossorigin src=\"", self$sources,"\"></script>",
+                             collapse = "\n")
+
+                    template_env$assets <-
+                      indent_level(source_tags, "sources")
+                  } else{
+                    template_env$sources <- ""
+                  }
+
+                  stringr::str_interp(self$template, template_env)
+                },
+
+                indent_to_level = function(text, element){
+                  ## find the indent level of the template element
+                  regex <- paste0("\\n\\s*\\$\\{",element,"\\}")
+                  elem_line <- stringr::str_extract(self$template, regex)
+                  indent_level <- stringr::str_count(elem_line,"\\s")
+
+                  ## insert indents after every newline in text.
+                  replacement <- paste0("\n",strrep(" ", indent_level))
+                  stringr::str_replace_all(text,
+                                           pattern = "\\n",
+                                           replacement = replacement)
+                },
+
+                serve = function(){},
+
+                write = function(){}
+              )
+              )
---
+++ tests/testthat/test_scene.R
@@ -0,0 +1,48 @@
+context("Test scene class")
+
+test_that("Scenes can read a built-in template",{
+  test_scene <- A_Scene$new(template = "basic")
+
+  expect_equal(
+  {
+    test_scene$template
+  },
+  {
+    ## the basic.html template
+    "<!DOCTYPE html>\n<html>\n    <head>\n    <meta charset=\"utf-8\">\n    <title> A test model</title>\n    <meta name=\"description\" content= \"A three js json model\">\n    <script crossorigin src=\"https://aframe.io/releases/0.8.0/aframe.min.js\"></script>\n    <script crossorigin src=\"https://cdn.rawgit.com/donmccurdy/aframe-extras/v4.0.2/dist/aframe-extras.loaders.js\"></script>\n    <script crossorigin src=\"https://cdn.rawgit.com/donmccurdy/aframe-extras/v4.0.2/dist/aframe-extras.controls.js\"></script>\n    <script crossorigin src=\"https://cdn.rawgit.com/donmccurdy/aframe-extras/v4.0.2/dist/aframe-extras.primitives.js\"></script>\n    ${sources}\n    </head>\n    <body>\n        <a-scene ${scene_components}>\n            <a-assets>\n                ${assets}\n            </a-assets>\n            <a-camera movement-controls=\"fly: true; easingY: 15\" position=\"0 1.8 0\"\n                      rotation=\"0 0 0\"></a-camera>\n            <a-entity position=\"0 100 -30\" light=\"intensity:0.80;type:point\"></a-entity>\n            \n            <!-- Entities added in R -->\n            ${entities}\n\n            <!-- Ground -->\n            <a-grid geometry='height: 10; width: 10'></a-grid>\n        </a-scene>\n    </body>\n</html>\n"
+  }
+  )
+
+})
+test_that("Scenes determine template indent levels correctly and apply indents",{
+  test_scene <- A_Scene$new(template = "basic")
+
+  expect_equal(
+  {
+    test_scene$indent_to_level("Hello\nWorld!","entities")
+  },
+  {
+    "Hello\n             World!"
+  }
+  )
+
+ })
+
+test_that("A scene can render the basic template",{
+
+  ## test with no entities
+  expect_equal(
+  {
+    my_empty_scene <- A_Scene$new()
+    cat(my_empty_scene$render())
+  },
+  {
+    ## The basic.html temple with blanks where the placeholders are.
+    "<!DOCTYPE html>\n<html>\n    <head>\n    <meta charset=\"utf-8\">\n    <title> A test model</title>\n    <meta name=\"description\" content= \"A three js json model\">\n    <script crossorigin src=\"https://aframe.io/releases/0.8.0/aframe.min.js\"></script>\n    <script crossorigin src=\"https://cdn.rawgit.com/donmccurdy/aframe-extras/v4.0.2/dist/aframe-extras.loaders.js\"></script>\n    <script crossorigin src=\"https://cdn.rawgit.com/donmccurdy/aframe-extras/v4.0.2/dist/aframe-extras.controls.js\"></script>\n    <script crossorigin src=\"https://cdn.rawgit.com/donmccurdy/aframe-extras/v4.0.2/dist/aframe-extras.primitives.js\"></script>\n    \n    </head>\n    <body>\n        <a-scene >\n            <a-assets>\n                \n            </a-assets>\n            <a-camera movement-controls=\"fly: true; easingY: 15\" position=\"0 1.8 0\"\n                      rotation=\"0 0 0\"></a-camera>\n            <a-entity position=\"0 100 -30\" light=\"intensity:0.80;type:point\"></a-entity>\n            \n            <!-- Entities added in R -->\n            \n\n            <!-- Ground -->\n            <a-grid geometry='height: 10; width: 10'></a-grid>\n        </a-scene>\n    </body>\n</html>\n"
+  }
+  )
+  ## with simple entities using assets and sources
+
+  ## with nested entities using assets and sources
+
+})
